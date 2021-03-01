5dc6df9c83e0693aa38f0ee3bfddf1debb305b4d
Roman Kinyakin <rk.grep@gmail.com>
2021-03-01 15:10:00 +0700

    Add exception and headers classes

---
+++ src/HeaderSelector.php
@@ -0,0 +1,78 @@
+<?php
+namespace Makini\Swagger;
+
+use \Exception;
+
+/**
+ * ApiException Class
+ * @package  Makini\Swagger
+ */
+class HeaderSelector
+{
+
+    /**
+     * @param string[] $accept
+     * @param string[] $contentTypes
+     * @return array
+     */
+    public function selectHeaders(array $accept, array $contentTypes): array
+    {
+        $headers = [];
+
+        $accept = $this->selectAcceptHeader($accept);
+        if ($accept !== null) {
+            $headers['Accept'] = $accept;
+        }
+
+        $headers['Content-Type'] = $this->selectContentTypeHeader($contentTypes);
+        return $headers;
+    }
+
+    /**
+     * @param string[] $accept
+     * @return array
+     */
+    public function selectHeadersForMultipart(array $accept): array
+    {
+        $headers = $this->selectHeaders($accept, []);
+
+        unset($headers['Content-Type']);
+        return $headers;
+    }
+
+    /**
+     * Return the header 'Accept' based on an array of Accept provided
+     *
+     * @param string[] $accept Array of header
+     *
+     * @return string Accept (e.g. application/json)
+     */
+    private function selectAcceptHeader(array $accept): ?string
+    {
+        if (count($accept) === 0 || (count($accept) === 1 && $accept[0] === '')) {
+            return null;
+        } elseif (preg_grep("/application\/json/i", $accept)) {
+            return 'application/json';
+        } else {
+            return implode(',', $accept);
+        }
+    }
+
+    /**
+     * Return the content type based on an array of content-type provided
+     *
+     * @param string[] $contentType Array fo content-type
+     *
+     * @return string Content-Type (e.g. application/json)
+     */
+    private function selectContentTypeHeader(array $contentType): string
+    {
+        if (count($contentType) === 0 || (count($contentType) === 1 && $contentType[0] === '')) {
+            return 'application/json';
+        } elseif (preg_grep("/application\/json/i", $contentType)) {
+            return 'application/json';
+        } else {
+            return implode(',', $contentType);
+        }
+    }
+}
